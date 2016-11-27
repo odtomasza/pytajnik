9602bb06414f450011ca0fa0ab80e1826d42e1b1
odtomasza <od.tomasza@gmail.com>
2016-11-27 17:59:20 +0100

    My first version app :)
    The next version of the script is shown in the same window...

---
+++ js.js
@@ -0,0 +1,21 @@
+function pytanie() {
+
+var imie=prompt("Podaj swoje imię");
+var wiek=prompt("Ile masz lat?");
+var potrawa=prompt("Jaka jest Twoja ulubiona potrawa?");
+var zainteresowania=prompt("Czym się interesujesz?");
+var lubie=prompt("Co lubisz?");
+
+
+
+    alert("Cześć nazywam się " + imie + " Mam lat " + wiek + 
+	" Moją ulubioną potrawą jest " + potrawa + " Moje zainteresowania to " + 
+	zainteresowania );
+
+if (true) {
+
+alert("Parę informacji o Tobie , Dziękuję za uwagę :)")
+}
+}
+
+console.log();
